ae7048cd2ac2c234652af6b58c8a5d9e3ee92253
Furo <furogaming9@gmail.com>
2024-03-23 12:34:10 +0100

    Home page items :eyes:

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,18 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <></>
+    <>
+    <div className="main2">
+      <div className="block">
+        <img className="afnArrow" draggable="false" src="https://afternight.dev/assets/arrow.png" width="50"/>
+        <h1> Project: Afternight</h1>
+      </div>
+      <div className="block">
+        <h2>A Roblox</h2>
+        <h2 className="title link" > Friday Night Funkin' </h2>
+        <h2>project made with passion.</h2>
+      </div>
+    </div>
+  </>
   )
 }
